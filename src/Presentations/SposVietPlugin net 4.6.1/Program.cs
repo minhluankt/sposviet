a3484f2dd9af7748e6505bf496480dd91c5b00fb
Minh Luận IT <95039035+minhluankt@users.noreply.github.com>
2023-06-01 22:41:29 +0700

    add

--- src/Presentations/SposVietPlugin net 4.6.1/Program.cs
+++ src/Presentations/SposVietPlugin net 4.6.1/Program.cs
@@ -67,7 +67,7 @@ namespace SposVietPlugin_net_4._6._1
                 //chen gia tri key
                 regkey.SetValue("Index", keyvalue);
                 //registryKey.SetValue("SposVietKySo", "");
-                registryKey.SetValue("SPOSVIET_PLUGIN", $"\"{Application.StartupPath}SposVietPlugin4.6.1.exe\"");
+                registryKey.SetValue("SPOSVIET_PLUGIN", $"\"{Application.StartupPath}\\SposVietPlugin4.6.1.exe\"");
                 //registryKey.SetValue("SPOSVIET-PLUGIN", Application.StartupPath + "SposVietPlugin_net_4._6._1.exe");
 
             }
